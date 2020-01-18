ef1281ff3c67350798ca618fdc9b17e9c14209be
manish <mkmaurya955@gmail.com>
2020-01-18 17:11:49 +0530

    commit

---
+++ mysite/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\sai\desktop\manish\python\online-examination\mysite\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
